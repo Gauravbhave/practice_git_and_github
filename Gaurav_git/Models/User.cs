3172f22a872b57ec82c4172ddc4ea97151622e0e
GauravBhave <gauravbhave890@gmail.com>
2026-02-13 10:35:25 +0530

    creating user model and inside creating user_id and user_name..

---
+++ Gaurav_git/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gaurav_git.Models
+{
+    public class User
+    {
+        [Key]
+        public int user_id { get; set; }
+
+        [Required]
+        public string user_name { get; set; }
+
+    }
+}
