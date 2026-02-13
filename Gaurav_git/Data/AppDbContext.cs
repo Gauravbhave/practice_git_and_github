4cb0f497fbfd9bad2f3f3db51c109696b33d88af
GauravBhave <gauravbhave890@gmail.com>
2026-02-13 17:42:06 +0530

    Creating appdbcontext in Data folder..

---
+++ Gaurav_git/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Gaurav_git.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gaurav_git.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
